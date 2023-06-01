5adeaa84670d53201a3b1673eb90f1e0bccd8558
alefajnypimpek <79838130+alefajnypimpek@users.noreply.github.com>
2023-06-01 16:05:56 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,42 @@
-const accordionItemHeaders = document.querySelectorAll(".accordion-item-header");
-
-accordionItemHeaders.forEach(accordionItemHeader => {
-  accordionItemHeader.addEventListener("click", event => {
-    
-    // Uncomment in case you only want to allow for the display of only one collapsed item at a time!
-    
-    // const currentlyActiveAccordionItemHeader = document.querySelector(".accordion-item-header.active");
-    // if(currentlyActiveAccordionItemHeader && currentlyActiveAccordionItemHeader!==accordionItemHeader) {
-    //   currentlyActiveAccordionItemHeader.classList.toggle("active");
-    //   currentlyActiveAccordionItemHeader.nextElementSibling.style.maxHeight = 0;
-    // }
-
-    accordionItemHeader.classList.toggle("active");
-    const accordionItemBody = accordionItemHeader.nextElementSibling;
-    if(accordionItemHeader.classList.contains("active")) {
-      accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
-    }
-    else {
-      accordionItemBody.style.maxHeight = 0;
-    }
-    
-  });
-});
+/** 
+ * Disable right-click of mouse, F12 key, and save key combinations on page 
+ */ 
+document.addEventListener("contextmenu", function(e){ 
+  e.preventDefault(); 
+  }, false); 
+  document.addEventListener("keydown", function(e) { 
+  //document.onkeydown = function(e) { 
+  // "I" key 
+  if (e.ctrlKey && e.shiftKey && e.keyCode == 73) { 
+  disabledEvent(e); 
+  } 
+  // "J" key 
+  if (e.ctrlKey && e.shiftKey && e.keyCode == 74) { 
+  disabledEvent(e); 
+  } 
+  // "S" key + macOS 
+  if (e.keyCode == 83 && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)) { 
+  disabledEvent(e); 
+  } 
+  // "U" key 
+  if (e.ctrlKey && e.keyCode == 85) { 
+  disabledEvent(e); 
+  } 
+  // "F12" key 
+  if (event.keyCode == 123) { 
+  disabledEvent(e); 
+  } 
+  // "C" key 
+  if (e.ctrlKey && event.keyCode == 67) { 
+  disabledEvent(e); 
+  } 
+  }, false); 
+  function disabledEvent(e){ 
+  if (e.stopPropagation){ 
+  e.stopPropagation(); 
+  } else if (window.event){ 
+  window.event.cancelBubble = true; 
+  } 
+  e.preventDefault(); 
+  return false; 
+}
